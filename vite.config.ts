e9813a58d2268430a605d23c65c44cb2dd8244cd
Assylbek Omarov <165684925+lunarisflos@users.noreply.github.com>
2025-04-23 23:34:55 +0200

    chore: prepare for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  // if your repo is at https://<you>.github.io/onboarding-journey
+  base: 'embark-onboarding-journey-main',
+  plugins: [react()],
+  build: {
+    outDir: 'docs'     // GitHub Pages can serve from /docs
+  }
+});
